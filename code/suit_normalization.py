091713e5d2cebbbf551d135d062ec2ac93031791
hspopal <haroonpopal@Haroons-iMac.local>
2026-07-03 22:43:57 -0400

    new suitpy

---
+++ code/suit_normalization.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 14:35:13 2026
+
+@author: haroonpopal
+"""
+
+# Import necessary packages
+import os
+import sys
+from nilearn import plotting
+import SUITPy as suit
+import nibabel as nib
+import ants
+import nilearn.plotting as  npl
+import matplotlib.pyplot as plt
+
+
+
+beta_test = True
+
+if beta_test:
+    subj = 'sub-010'
+    task = 'mdoors'
+    
+    print('---BETA TESTING---')
+    
+else:
+    subj = 'sub-'+str(sys.argv[1])
+    task = str(sys.argv[2])
+
+
+# Set directories
+bids_dir = '/Volumes/HP-SOC-DOOR/social_doors/'
+os.chdir(bids_dir)
+
+#all_runs_dir = bids_dir + 'derivatives/social_doors-nilearn/'
+    
+    
+# Isolate the cerebellum
+
+# This function generates isolation mask for the cerebellum based on a T1 scan
+subj_anat_prefix = bids_dir+'derivatives/fmriprep/'+subj+'/anat/'+subj+'_run-1_space-MNI152NLin2009cAsym_desc-preproc_T1w'
+suit.isolate(subj_anat_prefix+'.nii.gz')
+
+
+# Visualize the mask on the T1 image
+img = nib.load(subj_anat_prefix+'.nii.gz')
+mask = nib.load(subj_anat_prefix+'_cerebellum_dseg.nii.gz')
+plotting.plot_roi(img, mask)
+
+
+# This function normalizes a source image to the SUIT cerebellar template using a provided cerebellum
+results = suit.normalize(source_file = subj_anat_prefix+'.nii.gz', 
+                         mask_file = subj_anat_prefix+'_cerebellum_dseg.nii.gz')
+
+
+  
+
+# Load as NiBabel image
+norm_img = nib.load(subj_anat_prefix+'_space-SUIT.nii.gz')
+# Load SUIT template
+template_img = nib.load(bids_dir+'derivatives/SUITPy/templates/SUIT.nii')
+# Visualize the result
+npl.plot_anat(template_img, title="SUIT Template",  display_mode='yz',
+              cut_coords=(-60, -30), colorbar=False)
+npl.plot_anat(norm_img, title="Warped T1w", display_mode='yz',
+              cut_coords=(-60, -30), colorbar=False)
+
+
+
+# Reslice functional contrasts into SUIT space
+
+# Apply the reslice function
+src = 'sub-ex_task-fingerseq.nii.gz'
+deform = 'sub-ex_T1w_to-SUIT_mode-image_xfm.nii.gz'
+mask = 'sub-ex_T1w_cerebellum_dseg.nii.gz'
+img = suit.reslice_image(source_image = src,deformation = deform,mask = mask,voxelsize=2)
+
+# Save the resliced image under a new filename
+nib.save(img,'sub-ex_task-fingerseq_space-SUIT.nii.gz')
+
+
+
+
+
+
